482926009a1846c012149ebf98dccf3ba4747a82
Joao Daniel <joaodanieljr@gmail.com>
2021-12-16 21:46:11 -0300

    Criando os icones e storyBoard

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -11,9 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+   
+    
+    @IBAction func HardnessSelected(_ sender: UIButton) {
+        let buttonTitle = sender.titleLabel?.text
+        
+      
+    }
+    
+    
 }
 
